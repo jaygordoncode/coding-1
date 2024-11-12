a231621312aad78897fdd3babf48d037d29962e6
Jaylen Gordon <blue.whills24@gmail.com>
2024-11-12 14:27:25 +0000

    new async activity learning how to use strings and defining them on our own.

---
+++ unit3/ asyncActivityNov8.py
@@ -0,0 +1,16 @@
+print(number 1. String concatenation means the process of combinng two  or more strings into a single string )
+print("Hello" + "Jaylen")
+print( numnber 2. rules that are placed when naming variables where that names cannot start with numbers or symbols.)
+print(they have to begin with a letter.)
+print(The variable names are case-sensetive, so watch what you type.)
+print(the variable name can only contain alpha-numeric characters and underscores.)
+print(Camel case means every word besides the first is going to start with a capital letter)
+powerPoint = ''
+print(Pascal case mans that everyword begins with a capital letter)
+PowerPoint = ''
+print(And lastly the Snake Case every word is going to be seperated by a underscore.)
+Power_Point = ''
+print( a = 3 and it would et it to 300 )
+print(x = 10 y = 2 z=y*2 is 4)
+print( x is greater than 1 because its a bigger number)
+print( and z equals 4)
