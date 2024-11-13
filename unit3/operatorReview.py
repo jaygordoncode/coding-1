d7fb2dfff4b017f2e31303bc68302ba118368d0d
Jaylen Gordon <blue.whills24@gmail.com>
2024-11-13 14:35:28 +0000

    going over operaters aned the meaning of each one

---
+++ unit3/operatorReview.py
@@ -0,0 +1,42 @@
+# Artihmetic operator - writing code that does math processesw
+
+depositAmount = '50'
+withdrawAmount = '25'
+
+checkingAccount='1000'
+savingsAccount='2000'
+
+creditAmount = '1500'
+
+print(depositAmount + checkingAccount)
+
+parlayAmount ='.23'
+
+# Assignment operators- giving variables  values. KEYWORD = assignment 
+# any time you are using a variable, you are using the assignment operator 
+
+x= 5 
+x +=3
+
+
+teeShirt = 50.00
+
+discount = .25
+
+appliedDiscount = teeShirt * discount
+total = teeShirt - appliedDiscount
+
+print(total)
+
+# Comparison - analyzing two value to determine an outcome 
+
+wendysReviewScore = 4.0
+burgerKingReviewScore = 4.2
+
+wendysburgerCalories = 200
+burgerKingBurgerCalories = 300
+
+# compare best reviewed restuarant
+print(burgerKingReviewScore > wendysReviewScore )
+
+# comparing caloric intake 
