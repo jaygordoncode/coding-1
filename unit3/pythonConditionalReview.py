6ee4c1051dd4c0c6fe0b6278af12b7a658727714
Jaylen Gordon <blue.whills24@gmail.com>
2024-12-13 14:34:48 +0000

    async assignmnent

---
+++ unit3/pythonConditionalReview.py
@@ -0,0 +1,11 @@
+def grade_letter                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              V  (score):
+    if score >= 90:
+        print("You have an A")
+    elif score >= 80:
+        print("You have a B")
+    elif score >= 70:
+        print("You have a C")
+    elif score >= 60:
+        print("You have a D")
+    else:
+        print("You have an F")
