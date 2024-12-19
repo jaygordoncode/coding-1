5eb9d40a40564e5b735b0b43c02b9deb97cfe824
Jaylen Gordon <blue.whills24@gmail.com>
2024-12-19 14:36:48 +0000

    extra credit assignment for winter break

---
+++ unit3/holidayExtraCredit.py
@@ -0,0 +1,14 @@
+def holiday_schedule(date_input):
+    # A dictionary with the planned events
+    events = {
+        "12/21/2024": "chill",
+        "12/22/2024": "family dinner",
+        "12/23/2024": "decorate the Christmas tree",
+        "12/24/2024": "Christmas Eve celebration",
+        "12/25/2024": "Christmas Day lunch with family",
+        "12/31/2024": "New Year's party",
+        "1/1/2025": "New Year's brunch",
+        "1/7/2025": "returning back to school"
+    }
+    
+        print("Invalid date format. Use MM/DD/YYYY.")
