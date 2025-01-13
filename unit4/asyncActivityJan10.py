4dd99d11b2b69c7f2218387c6e856b4f6fdccf9a
Jaylen Gordon <blue.whills24@gmail.com>
2025-01-13 14:07:48 +0000

    async assignment for january 10th 2025

---
+++ unit4/asyncActivityJan10.py
@@ -0,0 +1,14 @@
+frozenFood = ["Frozen pizza", "Ice cream", "Frozen vegetables", "Chicken nuggets", "Frozen french fries"]
+internationalFood = ["Sushi rice", "Soy sauce", "Taco shells", "Curry paste", "Olive oil"]
+produce = ["Apples", "Carrots", "Broccoli", "Bananas", "Lettuce"]
+
+print("Frozen Food Isle:", frozenFood)
+print("International Food Isle:", internationalFood)
+print("Produce Isle:", produce)
+
+
+numbers = [1, 30, 39, 50, 293, 100]
+
+print(numbers[0])
+print(numbers[3])
+print(numbers[4])
